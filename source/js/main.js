a0f54e9c90d0650eedf9b8d14def74a1d5e1bced
Yuriy Frolov <yfrolou@mail.ru>
2025-11-20 14:08:45 +0300

    Добавляет частично разметку и стили header, hero. Подключает swiper

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,25 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  observer: true,
+  observeParents: true,
+});
+
