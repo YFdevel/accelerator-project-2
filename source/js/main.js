1612329b3036a1400c81b915be440f5041a7b61d
Yuriy Frolov <yfrolou@mail.ru>
2025-11-29 13:35:49 +0300

    Добавляет разметку и стили tours

--- source/js/main.js
+++ source/js/main.js
@@ -5,21 +5,59 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  mousewheel: true,
-  keyboard: true,
-  slidesPerView: 1,
-  spaceBetween: 0,
-  observer: true,
-  observeParents: true,
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('#swiper-hero', {
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: '#swiper-button-next-hero',
+      prevEl: '#swiper-button-prev-hero',
+    },
+    pagination: {
+      el: '#swiper-pagination-hero',
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    observer: true,
+    observeParents: true,
+  });
+  new Swiper('#swiper-tours', {
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: '#swiper-button-next-tours',
+      prevEl: '#swiper-button-prev-tours',
+    },
+    pagination: {
+      el: '#swiper-pagination-tours',
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    observer: true,
+    observeParents: true,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+    }
+  });
+});
+
+// Mobile Menu
+document.addEventListener('DOMContentLoaded', () => {
+  const mobileButton = document.querySelector('.header__menu-button');
+  const nav = document.querySelector('.header__nav');
+
+  mobileButton.addEventListener('click', (event) => {
+    event.target.classList.toggle('opened');
+    nav.classList.toggle('active');
+  });
 });
 
